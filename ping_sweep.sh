f3f9718152097d066e22ab58739270ca85255237
Tales Miguel <talesmz@hotmail.com>
2022-09-13 12:35:16 -0300

    fail if no target is given

--- ping_sweep.sh
+++ ping_sweep.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$1" == "" ]
+then
+echo "[ERROR] No target given. Please, inform a target IP address"
+echo "e.g.: $0 192.168.0"
+else
 for host in {1..254};do
 # -w1 == 1 second; -c1 == 1 shot
 # every address come with 64 bytes written
