3bdaf4c87503698ad064ecbfed153ef0b2083e74
Tales Miguel <talesmz@hotmail.com>
2025-02-16 04:40:40 -0300

    wait no processo

--- minix_results/script_testes.sh
+++ minix_results/script_testes.sh
@@ -6,11 +6,14 @@ TESTES="10 100 10000
 100 10000 100000
 200 15000 150000"
 
+# Contador do número do teste
 NUM_TESTE=1
 
 for PARAMS in $TESTES; do
     echo "Executando teste $NUM_TESTE..."
-    ./teste_processos $PARAMS > "resultados_lottery_$NUM_TESTE.txt"
+    ./teste_processos $PARAMS > "minix_results/resultados_lottery_$NUM_TESTE.txt"
+    # Garantir que o próximo teste só será iniciado após a finalização do anterior
+    wait
     echo "Teste $NUM_TESTE concluído!"
     NUM_TESTE=$((NUM_TESTE + 1))
 done
