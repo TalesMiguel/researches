eb04ad6c78f1eb32e262ab07f3d2259d8975fa9b
Tales Miguel <talesmz@hotmail.com>
2025-02-16 04:46:34 -0300

    bugfix

--- minix_results/script_testes.sh
+++ minix_results/script_testes.sh
@@ -11,9 +11,10 @@ NUM_TESTE=1
 
 for PARAMS in $TESTES; do
     echo "Executando teste $NUM_TESTE..."
-    ./teste_processos $PARAMS > "minix_results/resultados_lottery_$NUM_TESTE.txt"
-    # Garantir que o próximo teste só será iniciado após a finalização do anterior
-    wait
+    ./teste_processos $PARAMS > "minix_results/resultados_lottery_$NUM_TESTE.txt" &  # Executa em segundo plano
+    
+    wait  # Espera o processo terminar
+    
     echo "Teste $NUM_TESTE concluído!"
     NUM_TESTE=$((NUM_TESTE + 1))
 done
